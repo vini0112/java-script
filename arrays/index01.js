c6dda54575d32fc4fcdca97cd73b7c5021dfc411
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2024-03-30 21:50:47 -0300

    ok

--- arrays/index01.js
+++ arrays/index01.js
@@ -7,6 +7,8 @@ let valores = []
 
 function add(){
     if(isnumero(num.value) && !inlista(num.value, valores)){
+        valores.push(Number(num.value))
+
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado!`
         lista.appendChild(item)
@@ -14,7 +16,6 @@ function add(){
     }else{
         alert('Valor invalido ou não encontrado!')
     }
-
 
     num.value = ''
     num.focus()
@@ -34,4 +35,38 @@ function inlista(n, l){
         return  false
     }
 
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert('Digite um valor para poder finalizar!')
+    }else{
+        
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        
+        
+        for(let pos in valores){
+            soma += valores[pos]
+
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        let media = soma / tot
+
+        resp.innerHTML = ``
+        resp.innerHTML += `<p>Valores cadastrados: ${tot}</p>`
+        resp.innerHTML += `<p> O maior valor cadastrado: ${maior}</p>`
+        resp.innerHTML += `<p> O menor valor cadastrado: ${menor}</p>`
+        resp.innerHTML += `<p> A soma dos valores: ${soma}</p>`
+        resp.innerHTML += `<p>A média dos valores: ${media}</p>`
+
+    }
+    
+    
 }
